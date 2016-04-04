08790fed1ee6469a2627fa688859c59614b5ab98
Matt Elhotiby <hotiby@gmail.com>
2016-04-04 08:28:33 -0400

    Initial youtube search

---
+++ README.md
@@ -0,0 +1,11 @@
+# React Youtube search
+
+### Installing...
+```bash
+git clone git@github.com:railsdevmatt/redux-video-search.git
+cd video
+npm install
+npm start
+```
+
+Special thanks for this course https://www.udemy.com/react-redux/
---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import _ from 'lodash';
+
+const API_KEY = 'AIzaSyB_KbAH1LRr7mBoaR2-QYc-ngx4dGN8nnk';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surboards');
+  }
+
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render(){
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'))
